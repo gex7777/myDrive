2cb43c115f18ed579390da211c4fae1e3b999871
George <gex7777@gmail.com>
2023-02-06 08:08:40 +0530

    add google uth client 2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <GoogleOAuthProvider clientId="1020334812095-or4hr3q8qgq7skqohqfoelsn7aavlack.apps.googleusercontent.com">
+      <CssBaseline />
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
